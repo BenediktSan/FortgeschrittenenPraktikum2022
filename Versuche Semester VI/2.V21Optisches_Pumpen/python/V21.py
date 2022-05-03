3bfe985f9b83a3d4c3208fe55999c8e71a9e2ba9
Niklas <ndtudo26@gmail.com>
2022-05-03 12:55:50 +0200

    linearer Fit done

--- Versuche Semester VI/2.V21Optisches_Pumpen/python/V21.py
+++ Versuche Semester VI/2.V21Optisches_Pumpen/python/V21.py
@@ -19,7 +19,7 @@ if os.path.exists("build/plots") == False:
 ## Read in the measurements
 
 #Fequenzen der RF Spule in Hz:
-freq = np.array([1,2,3,4,5,6,7,8,9,10]) * 1e6
+freq = np.array([1,2,3,4,5,6,7,8,9,10]) * 1e5
 
 #Stromstärken der Sweep-Spule in A:
 i_sweep_1 = np.array([6.82,4.75,7.1,2.37,2.1,2.65,2.4,4.05,2.45,3.85]) * 0.1
@@ -48,6 +48,7 @@ n_ver = 20
 
 ##Konstanten einlesen
 mu = const.mu_0
+h  = const.h  
 
 
 
@@ -63,9 +64,27 @@ def Helm( I , R , N):
 B_1 = Helm(i_sweep_1 , r_sweep, n_sweep) + Helm(i_hor_1, r_hor, n_hor)
 B_2 = Helm(i_sweep_2 , r_sweep, n_sweep) + Helm(i_hor_2, r_hor, n_hor)
 
+##Fitten:
+
+p_arr_1 , cov_1 = np.polyfit(freq, B_1, deg=1, cov=True)
+print(p_arr_1)
+error_1 = np.sqrt(np.diag(cov_1))
+print(error_1)
+p_arr_2 , cov_2 = np.polyfit(freq, B_2, deg=1, cov=True)
+
+
+
+##Plotten:
+#Linspace:
+x_freq = np.linspace(1 * 1e5, 10 * 1e5, 1000)
+#Peak magnetfleder
 plt.figure()
 plt.plot(freq, B_1, '+', label='Isotop 1')
+plt.plot(x_freq, x_freq * p_arr_1[0] + p_arr_1[1], label='fit zu Isotop 1')
 plt.plot(freq, B_2, 'o', label='Isotop 2')
+plt.plot(x_freq, x_freq * p_arr_2[0] + p_arr_2[1], label='fit zu Isotop 2')
+plt.xlabel('Frequenz der RF-Spule in Hz')
+plt.ylabel('Summierte Magnetfeldstärke in den Peaks')
 plt.legend()
 plt.savefig('Magnetfeld.png')
 
