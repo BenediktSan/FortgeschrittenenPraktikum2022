f32b58f02822be65060b30cbb5aa80a7ec02265b
Niklas <ndtudo26@gmail.com>
2022-07-03 23:29:06 +0200

    makefile und Quellen

--- Versuche Semester VI/5.V1Lebensdauer_kosmischer_Myonen/python/V1.py
+++ Versuche Semester VI/5.V1Lebensdauer_kosmischer_Myonen/python/V1.py
@@ -26,14 +26,14 @@ if os.path.exists("build/plots") == False:
 ########Grafiken########
 
 
-plt.figure()
-plt.plot(f,U/(10),"x",label="Messwerte")
-plt.plot(x,func(x,omega0) )
-plt.xscale('log')
-plt.xlabel(r"$\Omega=\frac{\nu}{\nu_0}$")
-plt.ylabel(r"$\frac{U_Br}{U_0}$")
+#plt.figure()
+#plt.plot(f,U/(10),"x",label="Messwerte")
+#plt.plot(x,func(x,omega0) )
+#plt.xscale('log')
+#plt.xlabel(r"$\Omega=\frac{\nu}{\nu_0}$")
+#plt.ylabel(r"$\frac{U_Br}{U_0}$")
 #plt.xticks([5*10**3,10**4,2*10**4,4*10**4],[r"$5*10^3$", r"$10^4$", r"$2*10^4$", r"$4*10^4$"])
 #plt.yticks([0,np.pi/8,np.pi/4,3*np.pi/8,np.pi/2],[r"$0$",r"$\frac{\pi}{8}$", r"$\frac{\pi}{4}$",r"$\frac{3\pi}{8}$", r"$\frac{\pi}{2}$"])
-plt.tight_layout()
-plt.legend()
-plt.savefig("build/plots/plot1.pdf")
+#plt.tight_layout()
+#plt.legend()
+#plt.savefig("build/plots/plot1.pdf")
